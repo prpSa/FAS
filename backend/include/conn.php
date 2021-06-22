cdeee64318288d173db5abff08c72d10d5e06bac
unknown <pagarprasanna.2001@gmail.com>
2021-06-22 14:26:01 +0530

    FAS Heroku

--- backend/include/conn.php
+++ backend/include/conn.php
@@ -1,9 +1,9 @@
 <?php
     // Create connection
-    $server = "localhost";
-    $uname = "root";
-    $password = "";
-    $conn = mysqli_connect($server, $uname, $password,"fasdbnew");
+    $server = "remotemysql.com";
+    $uname = "wLAXeDdEcy";
+    $password = "zlxmJOQSFG";
+    $conn = mysqli_connect($server, $uname, $password,"wLAXeDdEcy");
     // Check connection
     if ($conn->connect_error)  {
         die("Connection failed: " . $conn->connect_error);
