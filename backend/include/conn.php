e7308878d064cd42d5c920e22d91b678cd3dffa3
unknown <pagarprasanna.2001@gmail.com>
2021-06-22 23:49:37 +0530

    Updated

--- backend/include/conn.php
+++ backend/include/conn.php
@@ -1,9 +1,9 @@
 <?php
     // Create connection
     $server = "remotemysql.com";
-    $uname = "wLAXeDdEcy";
-    $password = "zlxmJOQSFG";
-    $conn = mysqli_connect($server, $uname, $password,"wLAXeDdEcy");
+    $uname = "1CK1nZDjXi";
+    $password = "5r6okbJjIq";
+    $conn = mysqli_connect($server, $uname, $password,"1CK1nZDjXi");
     // Check connection
     if ($conn->connect_error)  {
         die("Connection failed: " . $conn->connect_error);
